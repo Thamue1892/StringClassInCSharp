87fc53fe246d44e3ce23c94103671209cb0cf7ac
Thaymes <thamue1892@gmail.com>
2018-02-14 01:53:02 +0200

    Formatting strings

---
+++ FormatStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FormatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input
+            Console.WriteLine("Enter a number ");
+            string input = Console.ReadLine();
+
+            //Convert the input string to an int.
+            int j;
+            Int32.TryParse(input, out j);
+
+            //Write a different string each iteration
+            string s;
+            for (int i = 0; i < 10; i++)
+            {
+                //Simple format string with no alignment formatting
+                s = string.Format("{0} times {1} = {2}", i, j, (i * j));
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- StringClassInCSharp/Program.cs
+++ StringClassInCSharp/Program.cs
@@ -9,6 +9,14 @@ namespace StringClassInCSharp
             //using regular strings
             string columns = "Column 1\tColumn 2\tColumn 3";
             Console.WriteLine(columns);
+            Console.WriteLine();
+
+            //using the verbatim strings
+            string title = @"My pensive SARA! thy soft cheek reclined
+                  Thus on mine arm,most soothing sweet it is
+                  To sit beside our Cot,...";
+
+            Console.WriteLine(title);
         }
     }
 }
