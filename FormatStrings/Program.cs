2bdd077af6ae94df9d28912e948b377201447839
Thaymes <thamue1892@gmail.com>
2018-02-14 02:38:06 +0200

    Accessing Individual Characters

--- FormatStrings/Program.cs
+++ FormatStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FormatStrings
 {
@@ -32,7 +33,31 @@ namespace FormatStrings
             //Index values are zero-based.
             int index = s3.IndexOf("q");
             Console.WriteLine("The index of q is: {0} ", index);
-            Console.ReadKey();
+
+            //Accessing Individual Characters
+            string s5 = "Printing backwards";
+            for (int i = 0; i < s5.Length; i++)
+            {
+                Console.Write(s5[s5.Length-i-1]);
+            }
+
+            Console.WriteLine();
+
+            //Using stringBuilder to modify individual characters
+            string question = "hOW DOES mICROSOFT wORD DEAL WITH THE cAPS lOCK KEY?";
+            StringBuilder sb = new StringBuilder(question);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (System.Char.IsLower(sb[i]) == true)
+                    sb[i] = System.Char.ToUpper(sb[i]);
+                else if (System.Char.IsUpper(sb[i]) == true)
+                    sb[i] = System.Char.ToLower(sb[i]);
+            }
+            //Store the new string .
+            string corrected = sb.ToString();
+            Console.WriteLine(corrected);
+
         }
     }
 }
