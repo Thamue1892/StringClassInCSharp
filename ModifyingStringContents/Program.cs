a953b31dd56c99da5f4c92e4e44cb2fca1371720
Thaymes <thamue1892@gmail.com>
2018-02-15 01:50:32 +0200

    Using String.Replace method

--- ModifyingStringContents/Program.cs
+++ ModifyingStringContents/Program.cs
@@ -2,8 +2,19 @@
 {
     class Program
     {
+        string searchFor;
+        string replaceWith;
         static void Main(string[] args)
         {
+           Program app = new Program();
+            string s = "The mountains are behind the clouds today.";
+
+            //Replace one substring with another with string.Replace
+            //Only exact matches are supported.
+            s = s.Replace("mountains", "peaks");
+            System.Console.WriteLine(s);
+
+
         }
     }
 }
