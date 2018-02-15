8c8a564e4283dde4b604ad67689be36c2aa34b23
Thaymes <thamue1892@gmail.com>
2018-02-15 02:05:15 +0200

    Using Regex.Replace

--- ModifyingStringContents/Program.cs
+++ ModifyingStringContents/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace ModifyingStringContents
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ModifyingStringContents
 {
     class Program
     {
@@ -6,7 +10,7 @@
         string replaceWith;
         static void Main(string[] args)
         {
-           Program app = new Program();
+            Program app = new Program();
             string s = "The mountains are behind the clouds today.";
 
             //Replace one substring with another with string.Replace
@@ -14,7 +18,29 @@
             s = s.Replace("mountains", "peaks");
             System.Console.WriteLine(s);
 
+            //Use Regex.Replace for more flexibility.
+            //Replace "the" or "The" with "many" or "Many".
+            //Using System.Text.RegularExpressions
+            app.searchFor = "the";
+            app.replaceWith = "many";
+            s = Regex.Replace(s, app.searchFor, app.replaceMatchCase, RegexOptions.IgnoreCase);
+            Console.WriteLine(s);
+        }
 
+        private string replaceMatchCase(Match m)
+        {
+            //Test whether the match is capitalized
+            if (Char.IsUpper(m.Value[0]) == true)
+            {
+                //Capitalize the replacement string
+                StringBuilder sb = new StringBuilder(replaceWith);
+                sb[0] = (char.ToUpper(sb[0]));
+                return sb.ToString();
+            }
+            else
+            {
+                return replaceWith;
+            }
         }
     }
 }
