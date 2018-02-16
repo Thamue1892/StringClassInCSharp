37411567d6b1701e7e20d0f4f5c6529bcad03d7f
ThaymesCoder <tmpofu@thami.co.za>
2018-02-16 16:35:00 +0200

    Using the overload of the Equals mehod that specifies a StringComparison.

---
+++ ComparingStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComparingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Internal strings that will never be localized.
+            string root = @"C:\users";
+            string root2 = @"C:\Users";
+
+            //Use the overload of the Equals mehod that specifies a StringComparison.
+            bool result = root.Equals(root2, StringComparison.Ordinal);
+
+            Console.WriteLine("Ordinal comparison: {0} and {1} are {2}", root, root2,
+                            result ? "equal." : "not equal.");
+
+            Console.WriteLine();
+
+            
+        }
+    }
+}
